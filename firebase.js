5b318383688b7c8a2f2861dfab5e6f77a986b9bf
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-06-28 20:28:19 -0500

    mainjs

--- firebase.js
+++ firebase.js
@@ -1,4 +1,56 @@
 
+$("#walmart-button").on("click", function(event) {
+
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    var search = $("#food-input").val();
+
+    // Here we construct our URL
+    var queryURL = "http://api.walmartlabs.com/v1/search?apiKey=avwfn326ass7ejz97edr7w49&ls&query=" + search;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      dataType: "jsonp"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+
+    // -----------------------------------------------------------------------
+
+  });
+
+  $("#food-button").on("click", function(event) {
+
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    var foodSearch = $("#food-input").val();
+
+
+    // Here we construct our URL
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + foodSearch;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function(response) {
+      console.log(response);
+      $("#food-view").text(JSON.stringify(response))
+    });
+
+
+    // -----------------------------------------------------------------------
+
+  });
+
+
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyD22zMFQLX0LSzrND6ccKiFtjQXo4rQFlg",
